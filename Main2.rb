041abe70f380570563f24355b879d8a6ed7bb71b
Josh Comer <joshjcomer@gmail.com>
2011-03-08 01:41:41 -0400

    Pieces can be added to board

--- Main2.rb
+++ Main2.rb
@@ -5,8 +5,12 @@ class Main < Qt::MainWindow
 
 	def initialize
 		super
+		@pieces = Array.new(9) {Array.new(9)}
+		
 
 		setWindowTitle 'GoGo Red Panda'
+		@turn = false
+		@move = 0
 		init_ui
 		resize 800,850
 		show
@@ -19,6 +23,19 @@ class Main < Qt::MainWindow
     	@label.geometry = Qt::Rect.new(0, 25, 800, 800)
     	@label.pixmap = Qt::Pixmap.new("resources/board.png")
 
+    	@pieces.each_with_index do |n, row|
+			n.each_with_index do |p, col|
+		    	@pieces[row][col] = Qt::Label.new(self)
+		    	@pieces[row][col].objectName = "#{row}#{col}"
+		    	@pieces[row][col].text = ''
+		    	xPos = (row*86) + 50
+				yPos = (col*86) + 75
+		    	@pieces[row][col].geometry = Qt::Rect.new(0,0,80,80)
+		    	@pieces[row][col].pixmap = Qt::Pixmap.new("")
+		    	@pieces[row][col].move xPos-40,yPos-40
+		    end
+		end
+
 		file = menuBar().addMenu "&File"
 		@newGame = file.addAction "New Game"
 		file.addSeparator
@@ -29,8 +46,48 @@ class Main < Qt::MainWindow
 		connect(@newGame, SIGNAL("triggered()"),self,SLOT("createGame()"))
 	end
 
+	def mousePressEvent(event)
+		puts "Mouse clicked #{event.pos.x}, #{event.pos.y-25}"
+		determineCoOrd(event.pos)
+	end
+
+	def determineCoOrd(pos)
+		x = (pos.x - 50) / 86
+		y = (pos.y - 50) / 86
+		#return [x,y]
+		puts "#{x}#{y}"
+		validateMove x,y
+	end
+
+	def validateMove(x,y)
+		if (true) then #check for move validity here
+			#@pieces[x][y] = Piece.new(@turn ? :black : :white, x, y, @move)
+			drawPiece(x,y)
+			@turn = !@turn
+			@move += 1
+		end
+	end
+
 	def createGame
 		puts "Game created"
+	end
+
+	def drawPiece(x,y)
+    	@pieces[x][y].pixmap = Qt::Pixmap.new("resources/#{@turn ? "white" : "black"}.png")
+	end
+end
+
+class Piece
+	attr_reader :colour
+	attr_reader :x
+	attr_reader :y
+	attr_reader :move
+
+	def initialize(colour,x,y,move)
+		@colour = colour
+		@x = x
+		@y = y
+		@move = move
 	end
 end
 
