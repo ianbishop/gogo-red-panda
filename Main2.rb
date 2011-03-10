5a8578a8864e65f3f48f2c68574914725a840c82
Josh Comer <joshjcomer@gmail.com>
2011-03-10 15:08:30 -0400

    Added a bit of placement logic to board, slightly cleaned up the code as well

--- Main2.rb
+++ Main2.rb
@@ -2,6 +2,7 @@ require 'Qt4'
 
 class Main < Qt::MainWindow
 	slots 'createGame()','make_move(int,int)'
+	attr_reader :game_moves
 
 	@@blank
 	@@black
@@ -9,6 +10,7 @@ class Main < Qt::MainWindow
 	def initialize
 		super
 		@pieces = Array.new(9) {Array.new(9)}
+		@game_moves = []
 		@@blank ||= Qt::Pixmap.new("")
 		@@black ||= Qt::Pixmap.new("resources/black.png")
 		@@white ||= Qt::Pixmap.new("resources/white.png")
@@ -21,23 +23,22 @@ class Main < Qt::MainWindow
 	end
 
 	def init_ui
-    	@label = Qt::Label.new(self)
-    	@label.objectName = "label"
-    	@label.text = ''
-    	@label.geometry = Qt::Rect.new(0, 25, 800, 800)
-    	@label.pixmap = Qt::Pixmap.new("resources/board.png")
+		@label = Qt::Label.new(self)
+		@label.objectName = "label"
+		@label.geometry = Qt::Rect.new(0, 25, 800, 800)
+		@label.pixmap = Qt::Pixmap.new("resources/board.png")
 
-    	@pieces.each_with_index do |n, row|
+		@pieces.each_with_index do |n, row|
 			n.each_with_index do |p, col|
-		    	@pieces[row][col] = Piece.new(self,row,col)
-				@pieces[row][col].objectName = "#{x}#{y}"
-				@pieces[row][col].geometry = Qt::Rect.new(0,0,80,80)
-				@pieces[row][col].pixmap = @@white
-		    	xPos = (row*86) + 50
+				@pieces[row][col] = piece = Piece.new(self,row,col)
+				piece.objectName = "#{x}#{y}"
+				piece.geometry = Qt::Rect.new(0,0,80,80)
+				drawPiece(piece,:blank)
+				xPos = (row*86) + 50
 				yPos = (col*86) + 75
-				@pieces[row][col].move xPos-40,yPos-40
-				connect(@pieces[row][col], SIGNAL("makeMove(int,int)"),self,SLOT("make_move(int,int)"))
-		    end
+				piece.move xPos-40,yPos-40
+				connect(piece, SIGNAL("makeMove(int,int)"),self,SLOT("make_move(int,int)"))
+			end
 		end
 
 		file = menuBar().addMenu "&File"
@@ -53,21 +54,24 @@ class Main < Qt::MainWindow
 	def clear_board
 		@pieces.each_with_index do |n, row|
 			n.each_with_index do |p, col|
-				@pieces[row][col].pixmap = @@blank
+				drawPiece(@pieces[row][col],:blank)
 			end
 		end
 	end
 
+	#SLOT make_move(int, int)
 	def make_move(x,y) 
 		puts "move made #{x},#{y}"
 		validateMove x,y
 	end
 
 	def validateMove(x,y)
-		if (true) then #check for move validity here
-			drawPiece(x,y)
-			#@turn = (@turn==:black) ? :white : :black
+		piece = @pieces[x][y]
+		if (piece.colour == :blank) then #check for move validity here
+			drawPiece(piece,@turn)
+			@game_moves << {:move => @move, :colour => @turn}
 			@move += 1
+			@turn = (@turn == :black) ? :white : :black
 		end
 	end
 
@@ -78,14 +82,25 @@ class Main < Qt::MainWindow
 		@move = 0
 	end
 
-	def drawPiece(x,y)
-    	@pieces[x][y].pixmap = @turn == :white ? @@white : @@black
+	private
+	def drawPiece(piece,change_to)
+		case change_to
+		when :black then
+			piece.pixmap = @@black
+			piece.colour = :black
+		when :white then
+			piece.pixmap = @@white
+			piece.colour = :white
+		when :blank then
+			piece.pixmap = @@blank
+			piece.colour = :blank
+		end
 	end
 end
 
 class Piece < Qt::Label
 	signals 'makeMove(int,int)'
-	attr_reader :colour
+	attr_accessor :colour
 	attr_reader :x
 	attr_reader :y
 	attr_reader :turn_num
@@ -98,8 +113,8 @@ class Piece < Qt::Label
 		@turn_num = turn_num
 	end
 
+	#emits SIGNAL makeMove(int,int)
 	def mousePressEvent(event)
-		puts "Mouse clicked #{@x}, #{@y}"
 		emit makeMove(@x,@y)
 	end
 end
